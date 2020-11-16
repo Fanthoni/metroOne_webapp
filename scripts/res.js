90527adab02f465dc79ea79ee32db123e0b57094
Jun mo An <jan31@my.bcit.ca>
2020-11-16 06:39:57 -0800

    created basic reservation form that stores the data to firebase in the collection: Reservation

---
+++ scripts/res.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.form')
+
+const addNewReservation = () => {
+    const reservation = {
+      name: form.name.value,
+      time: form.time.value,
+      groupSize: form.groupSize.value,            
+    }
+      res.collection('Reservations').add(reservation)
+      .then(() => {
+      // Reset the form values
+      form.name.value = "",
+      form.time.value = "",
+      form.groupSize.value = ""      
+  
+      alert('Your reservation has been successfully saved')
+      })      
+  }
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    addNewReservation()
+  })

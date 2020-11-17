760496fd43c3df3da19a67848e7fcd234b3c9db6
Ferrel Anthoni <ferrelanthoni@gmail.com>
2020-11-16 21:06:48 -0800

    completed the searchbar functionality. The anchor tag in the store list still need proper event listener (store.js).

--- scripts/store.js
+++ scripts/store.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   // Store the id of the element that triggers the event
   var callerID;
 
+  // From the directory
   $("area").click(function (event) {
       callerID = event.target.id;
       console.log(callerID);
@@ -11,7 +12,8 @@ $(document).ready(function () {
       localStorage.setItem("storeID", callerID);
   });
 
-  $("a").click(function (event) {
+  // From the store list
+  $('a[class="store"]').click(function (event) {
     callerID = event.target.id;
     console.log(callerID);
     alert(callerID);

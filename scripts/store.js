87848a794cb094b90b5cebc010ee1d0ffa421e08
Ferrel Anthoni <ferrelanthoni@gmail.com>
2020-11-24 18:32:21 -0800

    added back button functionality in the store page. (User coming from store list or favorite list will be directed accordingly depending on which page they were previously)

--- scripts/store.js
+++ scripts/store.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
 
   // Store the id of the element that triggers the event
@@ -6,18 +5,36 @@ $(document).ready(function () {
 
   // From the directory
   $("area").click(function (event) {
-      callerID = event.target.id;
-      console.log(callerID);
-      alert(callerID);
-      localStorage.setItem("storeID", callerID);
+    callerID = event.target.id;
+    console.log(callerID);
+    alert(callerID);
+    localStorage.setItem("storeID", callerID);
+
+    localStorage.setItem("backButton", JSON.stringify({
+      toAppear: 'false',
+      toDirect: ''
+    }))
   });
 
   // From the store list
-  $("ul").on("click", ".store", function (event){
+  $("ul").on("click", ".store", function (event) {
     callerID = event.target.id;
     console.log(callerID);
     localStorage.setItem("storeID", callerID);
-  });
-  
-});
 
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    console.log(page);
+
+    // Backbutton should appear at the store page if the user press store link from the store list
+    // or favorite list
+    localStorage.setItem("backButton", JSON.stringify({
+      toAppear: 'true',
+      toDirect: page
+    }))
+
+    var temp = JSON.parse(localStorage.getItem("backButton"));
+    alert(temp['toDirect']);
+  });
+
+});
